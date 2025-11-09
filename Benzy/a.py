587c536a1bce6cb80571a9fa980807ea17a4d873
JanPanjan <89231282@student.upr.si>
2025-11-09 01:15:59 +0100

    a little type hinting, renaming, refactoring

--- Benzy/a.py
+++
@@ -1,4 +0,0 @@
-from benzy import Benzy
-
-b = Benzy("55")
-b.draw_bs()
--- Benzy/benzy.py
+++ Benzy/benzy.py
@@ -2,7 +2,9 @@ import sys
 import networkx as nx
 import matplotlib.pyplot as plt
 
-Cdt = tuple[int, int]  # coordinate
+Crd = tuple[int, int]  # coordinate
+Vtx = int  # vertex
+CrdLst = dict[Vtx, Crd]  # list of coordinates for every vertex
 
 
 class Benzy():
@@ -11,11 +13,11 @@ class Benzy():
     """
 
     def __init__(self, bec: str) -> None:
-        self.bec = self.check_bec(bec)
+        self.bec: str = self.check_bec(bec)
         self.np = sum([int(d) for d in self.bec])  # perimiter nodes
         self.__x = 1
         self.__y = 1
-        self.moveset: list[Cdt] = [
+        self.moveset: list[Crd] = [
             (2 * self.__x, self.__y),    # 1, up-right
             (2 * self.__x, -self.__y),   # 2, down-right
             (0, -2 * self.__y),          # 3, down
@@ -23,10 +25,10 @@ class Benzy():
             (-2 * self.__x, self.__y),   # 5, up-left
             (0, 2 * self.__y),           # 6, up
         ]
-        self.graph = self.__graph_from_bec()
-        self.coordinates: dict[int, Cdt] = self.__calc_coord()
+        self.graph: nx.Graph = self.__graph_from_bec()
+        self.coordinates: CrdLst = self.__calc_coord()
 
-    def draw_bs(self):
+    def draw_bs(self) -> None:
         """
         Plots the benzenoid system.
         """
@@ -58,7 +60,35 @@ class Benzy():
         g.add_edge(self.np, 1)  # connect the graph
         return g
 
-    def __move(self, node: Cdt, move: Cdt) -> Cdt:
+    def __find_cdt(self, graph: CrdLst, coordinates: Crd) -> bool:
+        """
+        Finds a vertex with these coordinates (or not).
+        """
+        for edge_c in graph.values():
+            if edge_c == coordinates:
+                return True
+        else:
+            return False
+
+    def __find_vtx(self, v: Vtx) -> bool:
+        """
+        Finds vertex in the graph (or not)
+        """
+        return v in self.graph
+
+    def __add_cdt(
+            self,
+            node_id: int,
+            cds: CrdLst,
+            rotation: int,
+            previous: Crd
+    ) -> None:
+        move: Crd = self.moveset[rotation]
+        next: Crd = self.__move(previous, move)
+        cds[node_id] = next  # don't bother checking if they already exist...
+        return
+
+    def __move(self, node: Crd, move: Crd) -> Crd:
         """
         Moves node to new coordinates specified by move (change of x and y)
         """
@@ -67,6 +97,10 @@ class Benzy():
     def __next_rotation(self, r: int):
         """
         Gets the next valid rotation, moving to the beginning/end of moveset if necessary.
+
+        The first calculated coordinate will always be done by an up-right move.
+        First calculated coordinate after a new digit is read will always be done
+        with a move 2 steps back in the moveset.
         """
         r += 1
         match r:
@@ -77,47 +111,65 @@ class Benzy():
             case _:
                 return r
 
-    def __find_cdt(self, graph: dict[int, Cdt], coordinates: Cdt) -> bool:
-        """
-        Finds if there already exists an edge with these coordinates.
-        """
-        for edge_c in graph.values():
-            if edge_c == coordinates:
-                return True
-        else:
-            return False
+    def __search_nodes(self, clst: CrdLst) -> CrdLst:
+        pass
 
-    def __add(
-            self,
-            node_id: int,
-            graph: dict[int, Cdt],
-            rotation: int,
-            previous: Cdt
-    ) -> None:
-        move: Cdt = self.moveset[rotation]
-        next: Cdt = self.__move(previous, move)
-        graph[node_id] = next  # don't bother checking if they already exist...
+    def __fil_me_up(self, clst: CrdLst) -> CrdLst:
+        """
+        Fills up the coordinate list with missing edges and vertices, since
+        the original list contains only vertices and edges that form the
+        boundary.
+        It moves from top left-most coordinate, to bottom right-most. E.g.:
 
-    def __calc_coord(self) -> dict[int, Cdt]:
+                             ×
+            from this --> ×     ×
+                          |     |
+
+                            ...   ...
+
+                                |     |
+                                ×     × <-- to this
+                                   ×
+        """
+        pass
+
+    def __calc_coord(self) -> CrdLst:
         """
         Calculates coordinates for all nodes.
+
+        Coordinates take place in the standard x-y-axial coordinate system.
+        Each hexagon is composed of 6 coordinates. They follow the @self.moveset
+        sequence, meaning:
+
+               ×            (2,1)
+            ×     ×    (0,0)     (4,0)
+            |     |  =   |         |
+            ×     ×    (0,-2)    (4,-2)
+               ×            (2,-3)
+
+        From (0,0) you get to (2,1) by applying (+2,+1) or an up-right move.
+        From (2,1) you get to (4,0) by applying (+2,-1) or a down-right move, and
+        so on.
         """
         r = 0  # starting rotation
         i = 1  # starting node
-        sc: Cdt = (0, 0)  # starting coordinates
-        pos: dict[int, Cdt] = {i: sc}  # starting position
+        sc: Crd = (0, 0)  # starting coordinates
+        clst: CrdLst = {i: sc}  # starting position
 
         for digit in self.bec:
             digit = int(digit)
             for _ in range(digit):
                 i += 1
-                self.__add(i, pos, r, sc)
+                self.__add_cdt(i, clst, r, sc)
                 r = self.__next_rotation(r)
-                sc = pos[i]
-            # save values for next digit
+                sc = clst[i]
+            # store next starting direction
             r -= 2
 
-        return pos
+        # add missing edges and nodes to the list
+        clst = self.__fill_me_up(clst)
+
+        return clst
 
 
 if __name__ == "__main__":
