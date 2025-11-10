ac4dc1f9937bea7d292885aaf8eb88e289d1e6c3
JanPanjan <89231282@student.upr.si>
2025-11-10 14:29:04 +0100

    expanded names for clarity

--- Benzy/benzy.py
+++ Benzy/benzy.py
@@ -4,9 +4,9 @@ import networkx as nx
 from networkx import Graph
 import matplotlib.pyplot as plt
 
-Crd = tuple[int, int]  # coordinate
-Vtx = int  # vertex
-CrdLst = dict[Vtx, Crd]
+Coordinates = tuple[int, int]  # coordinate
+Vertex = int  # vertex
+CoordinateList = dict[Vertex, Coordinates]
 
 
 class CoordinateSystem():
@@ -17,7 +17,7 @@ class CoordinateSystem():
     def __init__(self, bec: str) -> None:
         self.__x = 1
         self.__y = 1
-        self.__moveset: list[Crd] = [
+        self.__moveset: list[Coordinates] = [
             (2 * self.__x, self.__y),    # 1, up-right
             (2 * self.__x, -self.__y),   # 2, down-right
             (0, -2 * self.__y),          # 3, down
@@ -25,70 +25,75 @@ class CoordinateSystem():
             (-2 * self.__x, self.__y),   # 5, up-left
             (0, 2 * self.__y),           # 6, up
         ]
-        # Check @__fill_me_up for more details
-        self.__hn_moveset: list[Crd] = [
-            self.__moveset[1],  # down-right
-            self.__moveset[2],  # down
-            self.__moveset[3],  # down-left
-            self.__moveset[5],  # up
-        ]
-        self.__hn_clst: CrdLst = {}
-        self.clst: CrdLst = {}
-        self.__calc_coord(bec)
+        self.__primary_coordinates: CoordinateList = {}  # primary nodes
+        self.coordinates: CoordinateList = {}
+        self.__calculate_coordinates(bec)
 
-    def __move(self, node: Crd, move: Crd) -> Crd:
+    def __move(self, node: Coordinates, move: Coordinates) -> Coordinates:
         """ Moves node to new coordinates specified by move (change of x and y) """
         return (node[0] + move[0], node[1] + move[1])
 
-    def __add(self,
-              node_id: int,
-              clst: CrdLst,
-              rotation: int,
-              p: None | Crd = None) -> None:
+    def __add(self, node_id: int, node_crd: Coordinates, clst: CoordinateList) -> None:
+        """ Adds new coordinate to coordinate list.  """
+        clst[node_id] = node_crd
+
+    def __add_from_coordinates(self,
+                               node_id: int,
+                               coordinates: CoordinateList,
+                               rotation: int,
+                               predecessor: None | Coordinates = None) -> None:
         """ Adds new coordinate to coordinate list, based on previous node and move step.  """
-        # predecessor
-        if p is None:
-            p = self.clst[node_id - 1]
-        move: Crd = self.__moveset[rotation]
-        new_crd: Crd = self.__move(p, move)
-        clst[node_id] = new_crd
+        if predecessor is None:  # predecessor
+            predecessor = self.coordinates[node_id - 1]
+        move: Coordinates = self.__moveset[rotation]
+        new_crd: Coordinates = self.__move(predecessor, move)
+        coordinates[node_id] = new_crd
         return
 
-    # TODO
+    def __is_primary(self, node: Coordinates) -> bool:
+        """ Checks is the given node fits the criteria for a primary node.
+
+        Primary nodes (denoted as PN) are nodes that fit 3 criteria:
+        (1) their x-coordinate is divisible by 2 and
+        (2) their y-coordinate is divisible by 3 and
+        (3) there is a node immeadiatelly below them (i.e. (x,y-2))
+
+                          (2,1)
+            this --> (0,0)     (4,0) <-- this
+                       |         |
+                     (0,-2)    (4,-2)
+                          (2,-3)
+
+        """
+        raise NotImplementedError
+
+        # TODO
     def __fill_me_up(self) -> None:
         """ Fills up the coordinate list with missing edges and vertices
 
         Parameters:
             `clst`: coordinate list for all nodes
-            `hclst`: coordinate list for all horitontal nodes
+            `hclst`: coordinate list for all primary nodes
         ---
         The original list contains only vertices and edges that form the boundary.
-        To fill up the system, it has to go through all horizontal nodes (denoted
-        as HN) and draws the missing edges. HN's are nodes that can be reached by
-        all moves except ur-right or down-left. In other words, they are:
+        To fill up the system, it has to go through all primary nodes (check
+        @__is_primary for details) and draws the missing edges.
 
-                        ×
-            this --> ×     × <-- this
-                     |     |
-            this --> ×     × <-- this
-                        ×
-
-        The algorithm moves through all levels from top left-most coordinate, to
-        bottom right-most. E.g.:
+        The algorithm moves through all levels from top left-most to bottom right-most
+        primary node. E.g.:
 
                              ×
             from this --> ×     ×
                           |     |
-
                             ...   ...
-
-                                |     |
                                 ×     × <-- to this
+                                |     |
+                                ×     ×
                                    ×
 
-        It checks if a neighbouring HN exists in this level (i.e. 4x right). In case
-        it does not, but there are still HN's left somewhere in this level, it moves
-        to the next HN. This makes sure, that benzenoid systems with gaps are properly
+        It checks if a neighbouring PN exists in this level (i.e. 4x right). In case
+        it does not, but there are still PN's left somewhere in this level, it moves
+        to the next PN. This makes sure, that benzenoid systems with gaps are properly
         filled. E.g.:
 
                              ×     ×                      ×     ×
@@ -107,15 +112,15 @@ class CoordinateSystem():
                        ×     ×     ×     ×     ×     ×      ×     ×
                           ×     ×     ×     ×     ×     ×      ×
 
-        Node 1 has 2 as it's neighbouring HN, so the hexagon is traced and any missing
-        edges drawn. Moving onto 2, it's neighbouring HN is 3, so the procedure repeats.
-        3 has no neighbouring HN, but the list still contains x, y and z, so it moves to
-        x. When it reaches z, there is no neighbouring HN and no HN's left in this level,
-        so it moves on to HN 4.
+        Node 1 has 2 as it's neighbouring PN, so the hexagon is traced and any missing
+        edges drawn. Moving onto 2, it's neighbouring PN is 3, so the procedure repeats.
+        3 has no neighbouring PN, but the list still contains x, y and z, so it moves to
+        x. When it reaches z, there is no neighbouring PN and no PN's left in this level,
+        so it moves on to PN 4.
         """
         raise NotImplementedError
 
-    def __next_rotation(self, r: int):
+    def __next_rotation(self, rotation: int):
         """ Gets the next valid rotation, moving to the beginning/end of moveset if necessary.
 
         The first calculated coordinate will always be done by an up-right move.
@@ -145,17 +150,17 @@ class CoordinateSystem():
 
             ...
         """
-        r += 1
-        match r:
+        rotation += 1
+        match rotation:
             case 6:
                 return 0
             case -1:
                 return 5
             case _:
-                return r
+                return rotation
 
-    # BUG: detecting HN nodes is not correct
-    def __calc_coord(self, bec: str) -> CrdLst:
+    # BUG: detecting PN's is not correct
+    def __calculate_coordinates(self, bec: str) -> CoordinateList:
         """ Calculates coordinates for all nodes.
 
         Coordinates take place in the standard x-y-axial coordinate system.
@@ -172,46 +177,48 @@ class CoordinateSystem():
         From (2,1) you get to (4,0) by applying (+2,-1) or a down-right move, and
         so on (check @__next_rotation for more details).
 
-        At the same time mark "horizontal nodes" for the purpose of filling up the
+        At the same time mark "primary" nodes" for the purpose of filling up the
         system after coordinates for boundary nodes are calculated (check @__fill_me_up
         for more details).
         """
-        r = 0  # starting rotation
-        i = 1  # starting node
-        sc: Crd = (0, 0)  # starting coordinates
-        self.clst: CrdLst = {i: sc}  # starting position
-        self.__hn_clst[i] = sc  # horizontal nodes
+        rotation = 0  # starting rotation
+        node_id = 1  # starting node
+        start_coordinates: Coordinates = (0, 0)  # starting coordinates
+        self.coordinates: CoordinateList = {node_id: start_coordinates}  # starting position
+        self.__primary_coordinates[node_id] = start_coordinates  # primary nodes
 
         for digit in bec:
             digit = int(digit)
             for _ in range(digit):
-                i += 1
-                # print("===========================================================================")
-                # print(f"digit: {digit}  i: {i}  r: {r}, move: {self.__moveset[r]}")
+                node_id += 1
+                print("===========================================================================")
+                print(f"digit: {digit}  node id: {node_id}  rotation: {
+                      rotation}, move: {self.__moveset[rotation]}")
 
-                self.__add(i, self.clst, r, None)
-
-                # print("---------------------------------------------------------------------------")
-
-                # BUG: fix this HN detection
-                if self.__moveset[r] in self.__hn_moveset:  # it found a HN
-                    # print(f"Found HN: i: {i}")
-                    self.__add(i, self.__hn_clst, r, self.clst[i - 1])
+                self.__add_from_coordinates(node_id, self.coordinates, rotation, None)
 
                 # print("---------------------------------------------------------------------------")
-                # print(f"clst: {self.clst}")
-                # print(f"clst: {self.__hn_clst}")
 
-                r = self.__next_rotation(r)
-                sc = self.clst[i]
+                # BUG: fix this PN detection
+                if self.__is_primary(self.coordinates[node_id]):
+                    # print(f"Found PN: i: {i}")
+                    self.__add_from_coordinates(
+                        node_id, self.__primary_coordinates, rotation, self.coordinates[node_id - 1])
+
+                # print("---------------------------------------------------------------------------")
+                # print(f"clst: {self.coordinates}")
+                # print(f"clst: {self.__primary_coordinates}")
+
+                rotation = self.__next_rotation(rotation)
+                start_coordinates = self.coordinates[node_id]
             # store next starting direction
-            r -= 2
+            rotation -= 2
 
         # add missing edges and nodes to the list
-        # clst = self.__fill_me_up(clst, hn_clst)
-        print(("HN list of coordinates:"))
-        pprint(self.__hn_clst)
-        return self.clst
+        # clst = self.__fill_me_up(clst, pn_clst)
+        print(("pN list of coordinates:"))
+        pprint(self.__primary_coordinates)
+        return self.coordinates
 
 
 class Benzy():
@@ -221,7 +228,7 @@ class Benzy():
 
     def __init__(self, bec: str) -> None:
         self.bec: str = self.check_bec(bec)
-        self.np = sum([int(d) for d in self.bec])  # perimiter nodes
+        self.perimiter_nodes = sum([int(d) for d in self.bec])  # perimiter nodes
         self.graph: Graph = self.__graph_from_bec()
         self.coordinates: CoordinateSystem = CoordinateSystem(self.bec)
 
@@ -229,7 +236,7 @@ class Benzy():
         """ Plots the benzenoid system.  """
         nx.draw(
             G=self.graph,
-            pos=self.coordinates.clst,
+            pos=self.coordinates.coordinates,
             with_labels=True,
             node_size=700,
             node_color="skyblue",
@@ -253,8 +260,8 @@ class Benzy():
     def __graph_from_bec(self) -> Graph:
         """ Creates a networkx graph.  """
         g = Graph()
-        g.add_edges_from([(i, i + 1) for i in range(1, self.np)])
-        g.add_edge(self.np, 1)  # connect the graph
+        g.add_edges_from([(i, i + 1) for i in range(1, self.perimiter_nodes)])
+        g.add_edge(self.perimiter_nodes, 1)  # connect the graph
         return g
 
 
