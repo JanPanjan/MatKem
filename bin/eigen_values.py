bff456d467bbf08efe300e9c1d223eb826dacd0b
JanPanjan <89231282@student.upr.si>
2025-12-04 15:05:41 +0100

    eigen

--- bin/eigen_values.py
+++ bin/eigen_values.py
@@ -2,11 +2,23 @@
 
 import numpy as np
 import networkx as nx
+from pprint import pprint
 
-g = nx.Graph([(1,2), (2,3), (3,4)])
+# g = nx.Graph([(1,2), (2,3), (3,4)])
+
+g = nx.Graph([
+    (1,2), (1,5), (1,8),
+    (2,3), (2,6),
+    (3,4),
+    (4,5),
+    (6,7),
+    (7,8)
+])
+
 A = nx.to_numpy_array(g)
-
 eigval, eigvec = np.linalg.eigh(A)
 
-print("\n" ,eigval)
-print("\n", eigvec)
+print("eig values:\n", eigval, end="\n\n")
+print("eig vectors:\n")
+for i, _ in enumerate(eigvec):
+    print(i+1, eigvec[:, i])
