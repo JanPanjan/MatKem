be5409659c0a8f188391f1a9babc91b21837a10c
JanPanjan <89231282@student.upr.si>
2025-12-10 18:58:09 +0100

    eigvals

--- bin/eigen_values.py
+++ bin/eigen_values.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 # g = nx.Graph([(1,2), (2,3), (3,4)])
 
-g = nx.Graph([
+AAA = nx.Graph([
     (1,2), (1,5), (1,8),
     (2,3), (2,6),
     (3,4),
@@ -15,10 +15,54 @@ g = nx.Graph([
     (7,8)
 ])
 
-A = nx.to_numpy_array(g)
-eigval, eigvec = np.linalg.eigh(A)
+benzene = nx.Graph([
+    (1,2),
+    (2,3),
+    (3,4),
+    (4,5),
+    (5,6),
+    (6,1),
+])
+c7 = nx.Graph([
+    (1,2),
+    (2,3),
+    (3,4),
+    (4,5),
+    (5,6),
+    (6,7),
+    (7,1),
+])
+cubane = nx.Graph([
+    (1,2),
+    (1,6),
+    (1,4),
+    (2,7),
+    (2,3),
+    (3,8),
+    (3,4),
+    (4,5),
+    (5,6),
+    (5,8),
+    (6,7),
+    (7,8),
+])
 
-print("eig values:\n", eigval, end="\n\n")
-print("eig vectors:\n")
-for i, _ in enumerate(eigvec):
-    print(i+1, eigvec[:, i])
+def eig(g: nx.Graph) -> None:
+    A = nx.to_numpy_array(g)
+    eigval, eigvec = np.linalg.eigh(A)
+    eigval, eigvec = (np.round(eigval, 2), np.round(eigvec, 2))
+
+
+    print("eig values:\n\n", eigval, end="\n\n")
+    print("eig vectors:\n")
+    for i, _ in enumerate(eigvec):
+        print(i, eigvec[:, i])
+    print("\n")
+
+if __name__ == "__main__":
+    print("-------------------- benzene --------------------")
+    eig(benzene)
+    print("-------------------- cubane --------------------")
+    eig(cubane)
+    print("-------------------- c7 --------------------")
+    eig(c7)
